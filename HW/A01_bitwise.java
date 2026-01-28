7924a6909b0009f98f7f3a3b035526f75140e5af
mohammadasim07 <asim.lpu07@gmail.com>
2026-01-28 23:21:28 +0530

    Day2 bitwise operator

---
+++ HW/A01_bitwise.java
@@ -0,0 +1,8 @@
+public class A01_bitwise{
+    public static void main(String[] arg){
+        int a = 5;
+        int b = 3;
+        System.out.println(a&b);
+        System.out.println(a|b);
+    }
+}
